5211f838b6246ece27ec5c948dfebcc5edbd7af7
treychelle_weathers <treychelle.weathers2017@gmail.com>
2023-01-14 15:49:12 -0500

    randomQuestion

--- game.js
+++ game.js
@@ -1,1 +1,239 @@
-console.log("hello world");
+//reference several pieces of the HTML elements using id's
+//to get a reference to the choice-texts/the answer choice - can give each an ID but there is a better way to do it,
+//so i will query on the class choice-text, that will give a list of choices, and will update from that point
+//also adding data-number to create a custom data attribute
+const question = document.getElementById("question");
+//creating an arrray from the HTML Collection list so that I can use array functions and manage the data - note that HTML needs to load before js or it won't have access to the data I am reference=ing
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
+
+let currentQuestion = [];
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+/*check to see if it is correct by comparing the answer number */
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "OH DOMMY! What is the significance of where you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "If you want to join Software junction, tell me difference between using a standard and an arrow function ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "Reference and calling may be similar, but if you think they're the same pass the defibulator...tell me baby engineer before you leave the incubator",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "A great engineer you are? Alas alas, not if you can't spot the difference between an object and a class!",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+    {
+        question: "DOMMY DOMMY! What is the significance of whhere you place the <script> mommy? ",
+        choice1: "it doesn't matter, because it is in the HTML",
+        choice2: "it doesn't matter, because the js is linked to the HTML and CSS",
+        choice3: "it does matter, because paages load in order and the HTML has to be loaded before the js can add functionality",
+        choice4: "it does matter, because the pages need to load the js first because functions take longer to load so it keeps your page fast",
+        answer: 3,
+    },
+];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionCounter ++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+};
+startGame();
+
+/* use Math.floor to return a random number for the random question - a random nunmber between 0-3. Math.random alone returns a  decimal between 0-1 */
